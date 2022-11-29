19cd29aeab2640583f47f12b6baa6b8f9eb147bd
Bálint <47783658+Roharx@users.noreply.github.com>
2022-11-29 13:31:58 +0100

    Cleaned up the code, put some methods from controller to model

--- src/mytunessys/gui/controller/PlaylistController.java
+++ src/mytunessys/gui/controller/PlaylistController.java
@@ -1,4 +1,36 @@
 package mytunessys.gui.controller;
 
-public class PlaylistController {
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import mytunessys.be.Playlist;
+import mytunessys.bll.LogicManager;
+
+/**
+ * @author Bálint & Matej
+ */
+
+public class PlaylistController extends BaseController {
+
+    private List<Playlist> playlists;
+
+    public PlaylistController(){
+        //playlists = getAllPlaylists();
+    }
+
+    private List<Playlist> getAllPlaylists(){ //waiting on backend to develop
+        //return logicManager.getAllPlaylists();
+        return null;
+    }
+
+    private void displayPlaylistsOnListView(){ //waiting on backend to develop
+        ObservableList<String> playlistNames = FXCollections.observableArrayList();
+        for (Playlist p : playlists) {
+            //playlistNames.add(p.getName());
+        }
+
+        contentList.setItems(playlistNames);
+    }
+
 }
