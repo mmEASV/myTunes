b4132f0b91a680f241e3c300d7ba4b5a203e883e
Matej Mazur <matejmazur@outlook.com>
2022-11-29 11:17:38 +0100

    Added sample List of Objects and added Song to show on initialization.

--- src/mytunessys/gui/controller/BaseController.java
+++ src/mytunessys/gui/controller/BaseController.java
@@ -1,19 +1,26 @@
 package mytunessys.gui.controller;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import mytunessys.be.Genre;
+import mytunessys.be.Playlist;
 import mytunessys.be.Song;
 
-public class BaseController {
+public class BaseController implements Initializable {
 
     public Label lblNameOfSong;
     public Label lblArtist;
@@ -45,9 +52,14 @@ public class BaseController {
 
     @FXML
     private void switchToSongInterface(ActionEvent actionEvent){
-        ShowInterface(actionEvent,"Songs");
         //TODO switch the ui to song with btnSongs
 
+        List<Object> items = new ArrayList<>();
+        items.add(new Song(1,"Song 1","","","",new Genre(1,"Name")));
+        items.add(new Song(2,"Song 2","","","",new Genre(1,"Name")));
+        items.add(new Song(3,"Song 3","","","",new Genre(1,"Name")));
+        items.add(new Song(4,"Song 4","","","",new Genre(1,"Name")));
+        ShowInterface(actionEvent,"Songs", items);
         //change list to display songs
 
 
@@ -63,13 +75,24 @@ public class BaseController {
     }
     @FXML
     private void switchToPlaylistInterface(ActionEvent actionEvent){
-        ShowInterface(actionEvent,"Playlists");
+        List<Object> items = new ArrayList<>();
+        items.add(new Playlist());
+        ShowInterface(actionEvent,"Playlists",items);
         //TODO switch the ui to playlist with btnPlaylists
     }
 
-    public void ShowInterface(ActionEvent actionEvent,String name) {
+    public void ShowInterface(ActionEvent actionEvent, String name, List<Object> items) {
         btnGoBack.setVisible(false);
         lblCurrentLocation.setText(name);
+        ObservableList<Object> songs = FXCollections.observableArrayList();
+        if(items != null)
+            songs.addAll(items);
+        contentList.setItems(songs);
+    }
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        switchToSongInterface(new ActionEvent());
     }
 }
