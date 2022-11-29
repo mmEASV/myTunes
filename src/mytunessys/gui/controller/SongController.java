dd4ccaf4604c4071801575cc1a6be7b22a39b139
Bálint <47783658+Roharx@users.noreply.github.com>
2022-11-29 14:25:25 +0100

    Switched from ListView to TableView and altered the code for it

--- src/mytunessys/gui/controller/SongController.java
+++ src/mytunessys/gui/controller/SongController.java
@@ -12,20 +12,7 @@ import mytunessys.be.Song;
  */
 public class SongController extends BaseController{
 
-    private List<Song> songs;
 
 
-    private List<Song> getAllSongs() {
-        return null;
-    }
-
-    private void displaySongsOnListView(){
-        ObservableList<String> songTitles = FXCollections.observableArrayList();
-        for (Song s : songs) {
-            songTitles.add(s.getTitle());
-        }
-
-        contentList.setItems(songTitles);
-    }
 
 }
