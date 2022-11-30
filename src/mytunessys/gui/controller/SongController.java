56892ad867878ff41355f09595ffd84e53f66cec
Matej Mazur <matejmazur@outlook.com>
2022-11-30 10:50:39 +0100

    Changing The ButtonSize

--- src/mytunessys/gui/controller/SongController.java
+++ src/mytunessys/gui/controller/SongController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import mytunessys.be.Genre;
 import mytunessys.be.Playlist;
 import mytunessys.be.Song;
 
@@ -17,6 +19,8 @@ public class SongController{
 
     public void Show(AnchorPane centerContent){
         var Table = new TableView<>();
+        Table.setLayoutX(14);
+        Table.setFocusTraversable(false);
 
         var TitleCollumn = new TableColumn<>();
         TitleCollumn.setText("Title");
@@ -24,12 +28,15 @@ public class SongController{
 
         var GenreCollumn = new TableColumn<>();
         GenreCollumn.setText("Genre");
+        GenreCollumn.prefWidthProperty().set(47);
 
         var DurationCollumn = new TableColumn<>();
         DurationCollumn.setText("Duration");
+        DurationCollumn.prefWidthProperty().set(47);
 
         var OptionsCollumn = new TableColumn<>();
         OptionsCollumn.setText("Options");
+        OptionsCollumn.prefWidthProperty().set(47);
 
         Table.editableProperty().set(false);
         Table.getColumns().add(TitleCollumn);
