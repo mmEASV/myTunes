f7ccfc30b1020c94a9e733a1c24b1a29dcdde9c4
Bálint <47783658+Roharx@users.noreply.github.com>
2022-11-30 11:21:42 +0100

    Fixed the design of Playlist and corrected spelling errors in the code.

--- src/mytunessys/gui/controller/SongController.java
+++ src/mytunessys/gui/controller/SongController.java
@@ -1,16 +1,8 @@
 package mytunessys.gui.controller;
 
-import java.util.List;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
-import mytunessys.be.Genre;
-import mytunessys.be.Playlist;
-import mytunessys.be.Song;
 
 /**
  * @author Bálint & Matej
@@ -22,27 +14,27 @@ public class SongController{
         Table.setLayoutX(14);
         Table.setFocusTraversable(false);
 
-        var TitleCollumn = new TableColumn<>();
-        TitleCollumn.setText("Title");
-        TitleCollumn.prefWidthProperty().set(203);
+        var TitleColumn = new TableColumn<>();
+        TitleColumn.setText("Title");
+        TitleColumn.prefWidthProperty().set(203);
 
-        var GenreCollumn = new TableColumn<>();
-        GenreCollumn.setText("Genre");
-        GenreCollumn.prefWidthProperty().set(47);
+        var GenreColumn = new TableColumn<>();
+        GenreColumn.setText("Genre");
+        GenreColumn.prefWidthProperty().set(47);
 
-        var DurationCollumn = new TableColumn<>();
-        DurationCollumn.setText("Duration");
-        DurationCollumn.prefWidthProperty().set(47);
+        var DurationColumn = new TableColumn<>();
+        DurationColumn.setText("Duration");
+        DurationColumn.prefWidthProperty().set(47);
 
-        var OptionsCollumn = new TableColumn<>();
-        OptionsCollumn.setText("Options");
-        OptionsCollumn.prefWidthProperty().set(47);
+        var OptionsColumn = new TableColumn<>();
+        OptionsColumn.setText("Options");
+        OptionsColumn.prefWidthProperty().set(47);
 
         Table.editableProperty().set(false);
-        Table.getColumns().add(TitleCollumn);
-        Table.getColumns().add(GenreCollumn);
-        Table.getColumns().add(DurationCollumn);
-        Table.getColumns().add(OptionsCollumn);
+        Table.getColumns().add(TitleColumn);
+        Table.getColumns().add(GenreColumn);
+        Table.getColumns().add(DurationColumn);
+        Table.getColumns().add(OptionsColumn);
         Table.setFocusTraversable(false);
         centerContent.getChildren().add(Table);
     }
