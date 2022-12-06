1e5963132cbe27d347fde91a5e5e7feb7d1c8566
TomassSimko <simko.t@email.cz>
2022-12-06 13:51:15 +0100

    this commit added fixed width to our alert box

--- src/mytunessys/bll/utilities/AlertNotification.java
+++ src/mytunessys/bll/utilities/AlertNotification.java
@@ -2,6 +2,9 @@ package mytunessys.bll.utilities;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Region;
+import javafx.stage.StageStyle;
 
 import java.util.Optional;
 
@@ -11,6 +14,14 @@ public class AlertNotification {
         alert.setTitle(title);
         alert.setHeaderText(null);
         alert.setContentText(content);
+        alert.getDialogPane().getChildren()
+                .stream()
+                .filter(node -> node instanceof Label)
+                .forEach(node -> ((Label)node)
+                        .setMinHeight(Region.USE_PREF_SIZE));
+        alert.setResizable(false);
+        alert.getDialogPane().setMaxWidth(350);
+        alert.initStyle(StageStyle.UNDECORATED);
         return alert.showAndWait();
     }
 }
