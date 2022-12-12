1cd30a02d5a3f157abf7e60ea5e763c14ec2ca31
TomassSimko <simko.t@email.cz>
2022-12-13 00:36:22 +0100

    this commit added interface for connection and implemented abstraction to its dao so more abstration added

--- src/mytunessys/dal/repository/GenreDAO.java
+++ src/mytunessys/dal/repository/GenreDAO.java
@@ -2,7 +2,9 @@ package mytunessys.dal.repository;
 
 
 import mytunessys.be.Genre;
-import mytunessys.dal.connectors.MSSQLConnection;
+import mytunessys.bll.types.DatabaseType;
+import mytunessys.dal.AbstractConnectionFactory;
+import mytunessys.dal.ConnectionFactory;
 import mytunessys.dal.mappers.GenreMapper;
 import mytunessys.dal.repository.interfaces.IGenreDAO;
 
@@ -18,17 +20,17 @@ import java.util.List;
 
 public class GenreDAO implements IGenreDAO {
 
-    private final MSSQLConnection mssqlConnection;
+    AbstractConnectionFactory mssqlFactory;
 
     public GenreDAO() throws Exception{
-        this.mssqlConnection = new MSSQLConnection();
+        this.mssqlFactory = ConnectionFactory.getFactory(DatabaseType.MSSQL);
     }
 
     @Override
     public List<Genre> getAllGenre() throws Exception {
         GenreMapper mapper = new GenreMapper();
         List<Genre> retrievedGenreList = new ArrayList<>();
-        try (Connection connection = mssqlConnection.createConnection()) {
+        try (Connection connection = mssqlFactory.createConnection()) {
             String sql = "SELECT id,genre_name FROM genre";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
